901f47084dc3446f13fa0993d1913de7a521a324
Luis Zabala <luis.zabala076@gmail.com>
2019-01-30 17:36:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,4 @@
-8require 'pry'
+require 'pry'
 
 class Owner
   
@@ -31,7 +31,7 @@ class Owner
   
   def list_pets
     pets[:pets]
-    "I have #{fish.count} fish, #{dogs.count} dog(s), #{cats.count} cat(s)."
+    "I have #{fish.count} fish, #{dog.count} dog(s), #{cat.count} cat(s)."
   end
   
   
